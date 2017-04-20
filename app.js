c17085e40f93b3ea9aef562b00d49b9d1ff0f2a3
Gary W <gary.wilpizeski@gmail.com>
2017-04-20 12:58:29 -0400

    routes + db

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const mongoose = require("mongoose");
+const config = require("./config/database")
 
-mongoose.connect();
+//Connect to database
+mongoose.connect(config.database);
+
+//On connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+});
+//On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error:  ' + err);
+});
 
 const app = express();
 
--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 //Register
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
     res.send('REGISTER');
 });
 
 //Authenticate
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
     res.send('AUTHENTICATE');
 });
 
 //Profile
 router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
-});
-
-//Validate
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE');
 });
 
 module.exports = router;
